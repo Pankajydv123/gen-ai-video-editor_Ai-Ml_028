c63d72b0c016bc55d2e6b72cdb1a78028250ef91
Pankajydv123 <ee240002048@iiti.ac.in>
2025-06-05 19:07:54 +0530

    Prepare for GitHub Pages deploy

--- frontend/react_gen/src/App.js
+++ frontend/react_gen/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Body from './components/body';
+// import Body from './components/body';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
     <Navbar title="GenAi"/>
-    <Body />
+    {/* <Body /> */}
     </>
   );
 }
