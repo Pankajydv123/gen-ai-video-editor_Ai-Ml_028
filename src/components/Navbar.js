456658d74ab1f5d8cc963e62e724c40052cdd172
Pankajydv123 <ee240002048@iiti.ac.in>
2025-06-06 12:50:44 +0530

    Prepare for GitHub Pages deploy

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Navbar.css';
-
+import './title.css';
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg fantastic-gradient" style={{
-  padding: '1rem 2rem',
+  padding: '1rem 2rem 1rem 0.5rem',
   boxShadow: '0 4px 20px rgba(0,0,0,0.05)',
   borderBottom: '1px solid #e0e0e0',
   position: 'sticky',
   top: 0,
   zIndex: 1000,
 }}>
-      <div className="container-fluid">
-        <a className="navbar-brand fw-bold" href="/" style={{ fontSize: '1.5rem', color: 'rgb(9, 116, 100)' }}>
+      <div className="container-fluid" id="Tat">
+        <div className="tat">
+          <a className="navbar-brand fw-bold" id="Tit" href="/">
           {props.title}
         </a>
 
@@ -29,25 +30,27 @@ export default function Navbar(props) {
           <span className="navbar-toggler-icon"></span>
         </button>
 
+        </div>
+        
         <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{ gap: '1rem' }}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/" style={{ color: 'rgb(9,116,100)' }}>
+              <a className="nav-link active" aria-current="page" href="/" >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/" style={{ color: 'rgb(9,116,100)'}}>
+              <a className="nav-link" href="/" >
                 Features
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/" style={{ color: 'rgb(9 116 100)' }}>
+              <a className="nav-link" href="/" >
                 Pricing
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/" style={{ color: 'rgb(9,116,100)' }}>
+              <a className="nav-link" href="/" >
                 About
               </a>
             </li>
