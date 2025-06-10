3fd21b0230835854de62a166daa0294d0edb22bf
Pankajydv123 <ee240002048@iiti.ac.in>
2025-06-10 08:38:46 +0530

    Prepare for GitHub Pages deploy

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Navbar from './components/Navbar';
 import About from './components/About';
 import Features from './components/Features';
 import Body from './components/body';
-// Remove unused Link import
+import Image_editor from './components/image_editor';
+import Video_editor from './components/video_editor';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" element={<Body />} />
           <Route path="/features" element={<Features />} />
           <Route path="/about" element={<About />} />
+          <Route path="/image_editor" element={<Image_editor />} />
+          <Route path="/video_editor" element={<Video_editor />} />
         </Routes>
     </>
   );
