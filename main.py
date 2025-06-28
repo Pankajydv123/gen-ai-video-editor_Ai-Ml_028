7fb64a51e191781e7505f2d95c12f3e09625299e
Abhishek1175 <mems240005038@iiti.ac.in>
2025-06-28 20:07:25 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
 
-app = FastAPI()
+from moviepy.editor import VideoFileClip
+mp4_file =input("Enter the full path of your video file")
+mp3_file = "audio.mp3"
+video_clip = VideoFileClip(mp4_file)
+audio_clip = video_clip.audio 
+audio_clip.write_audiofile(mp3_file)
+audio_clip.close()
+video_clip.close()
+print("Audio extraction successful!",mp3_file)
+import whisper
 
-@app.get("/")
-def read_root():
-    return {"message": "Backend Working"}
+model = whisper.load_model("tiny") 
+
+result = model.transcribe(mp3_file)
+print("Transcription:")
+print(result["text"])
+from bark import generate_audio, SAMPLE_RATE
+import scipy.io.wavfile as wavfile
+
+text_prompt = result
+
+audio_array = generate_audio(text_prompt)
+wavfile.write("bark_output.wav", SAMPLE_RATE, audio_array)
+print("Voice generated and saved as bark_output.wav")
