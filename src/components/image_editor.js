41c07a0f583c5d9c4a159ebcc83a105512beb4df
Saharsh525 <ee240002063@iiti.ac.in>
2025-07-02 00:44:16 +0530

    Update image_editor.js

--- src/components/image_editor.js
+++ src/components/image_editor.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import ImageEditor from './ImageEditor';
 
-export default function ImageEditor() {
+function App() {
   return (
     <div>
-        <h1>hello i am image editor feature</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam deserunt tenetur fugiat quod earum nisi praesentium, expedita odit assumenda sint magni adipisci ut iure ratione nesciunt, accusantium deleniti vitae repellat alias iusto. Reprehenderit ea architecto beatae eius totam laboriosam, cupiditate et neque voluptatem maxime nam alias, veritatis nisi impedit non voluptas amet mollitia quaerat aspernatur molestiae facilis facere dicta ipsa fugiat. Exercitationem placeat fugit dolore. Facilis tenetur minima minus architecto id, optio veritatis deleniti pariatur illo expedita rerum dolorem et ut maxime ipsum eligendi maiores perferendis sequi quas culpa exercitationem reprehenderit nisi quo facere. Nobis, obcaecati maxime, sit mollitia praesentium nihil rerum autem vel cumque, qui numquam tempore! At quae ratione, iusto doloremque voluptatibus dicta unde soluta necessitatibus blanditiis voluptas. Molestias pariatur deleniti veritatis nobis voluptas, quis dolore ex laborum ut! Obcaecati accusamus aperiam nam iste dolorum rem excepturi eaque quidem possimus. Perspiciatis ut quis, nobis tenetur harum nulla facilis ipsum accusantium illo mollitia aliquid aliquam ullam voluptate nesciunt aut suscipit recusandae! Alias voluptas, consectetur autem eum veniam, itaque id doloribus aliquid ipsam eaque amet unde perspiciatis voluptatem consequatur ipsum sapiente vero optio nihil culpa, incidunt nobis maiores ab provident earum. Nam odio ullam nostrum qui facere nobis dolorem perspiciatis.</p>
+      <h1>React Image Editor</h1>
+      <ImageEditor />
     </div>
-  )
+  );
 }
+
+export default App;
